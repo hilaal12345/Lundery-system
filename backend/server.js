cb6fc944d8e14a00964b47455e6ec40b93839946
hilaal12345 <hibaalibile29@gmail.com>
2025-09-12 12:40:01 +0300

    update backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const cors=require("cors")
+const mongoose  = require("mongoose")
+require("dotenv").config()
+
+
+const app =express()
+app.use(express.json())
+app.use(cors())
+const PORT=process.env.port || 3000
+
+mongoose.connect(process.env.db_Url).then(()=>console.log("sucsess concection"))
+
+// app.use("/allImages",express.static("docment"))
+
+
+
+
+app.listen(PORT,()=>console.log(`server is runing ${PORT}`))
