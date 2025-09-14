a421ab560a3222ae733e15f20449afe754662a2f
hilaal12345 <hibaalibile29@gmail.com>
2025-09-14 20:25:57 +0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,29 @@
-import OrderForm from "./order"
+// import OrderForm from "./order"
 
-// function App(){
-//   return <>
-//   <h1 className="text-7xl text-red-500"> Hello guys</h1>
-//   <p>banaat </p>
-//   <OrderForm/>
+// // function App(){
+// //   return <>
+// //   <h1 className="text-7xl text-red-500"> Hello guys</h1>
+// //   <p>banaat </p>
+// //   <OrderForm/>
 import Footer from "./components/footer"
 import Header from "./components/header"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/home"
 import Service from "./pages/services"
+import OrderForm from "./order"
 
 function App(){
-  return <>
-    <Header/>
-    <Footer/>
+  return <> 
+  <Header/>
+   
+    
    
     
     
     <Routes>
       {/* salmo  inta routekada kusameeso */}
       <Route path="/"element={<Home/>}/>
-      <Route path="/service"element={ <Service/>}/>
+      <Route path="/service"element={ <Service/>}/> 
 
 
 
@@ -32,9 +34,13 @@ function App(){
 
 
         {/*hibo  inta routekada kusameeso */}
+         <Route path="/OrderForm"element={<OrderForm/>}/> 
+         
 
       
     </Routes>
+
+    <Footer/>
     
 
 
--- frontend/src/pages/services.jsx
+++ frontend/src/pages/services.jsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom"
+
 function Service(){
           return <>
           <h1>Hello service</h1>
+         <Link to={"/OrderForm"}> <button className="bg-red-700 px-12 py-3"> clicl me</button></Link>
+
           
           
           </>
